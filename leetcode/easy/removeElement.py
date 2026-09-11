3b7339e87ca843c2b02c8a6c11f7a56e9f47ff2b
Kona <mahinakterkona.01@gmail.com>
2026-09-11 23:05:41 +0600

    Add Remove Element solution

---
+++ leetcode/easy/removeElement.py
@@ -0,0 +1,21 @@
+"""
+LeetCode 27: Remove Element
+Problem Link: https://leetcode.com/problems/remove-element/description/?envType=study-plan-v2&envId=top-interview-150
+Difficulty: Easy
+"""
+
+class Solution:
+    def remove_element(self , nums:list[int] , val:int)->int :
+        i=0
+        for j in range(len(nums)) :
+            if val!=nums[j] :
+                nums[i]=nums[j]
+                i+=1
+
+        return i
+
+if __name__ == "__main__" :
+    sol=Solution()
+    list=[1,2,3,4,2,6,7]
+    x=sol.remove_element(list,2)
+    print(f"Output array : {list[:x]}")
